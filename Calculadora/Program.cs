b681883b050935af66de8ff89eff803a90366685
GBrandao04 <gabrielmartbrandao@gmail.com>
2023-03-03 22:00:46 -0300

    Add project files.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,15 @@
+﻿float x, y;
+
+Console.WriteLine("Insira o primeiro número: ");
+x = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Insira o segundo número: ");
+y = float.Parse(Console.ReadLine());
+
+Console.WriteLine("A soma dos números é: " + (x+y));
+Console.WriteLine("A diferença dos números é: " + (x - y));
+Console.WriteLine("O produto dos números é: " + x * y);
+if(y == 0)
+    Console.WriteLine("Impossível dividir por 0");
+else
+    Console.WriteLine("O quociente dos números é: " + (x / y));
